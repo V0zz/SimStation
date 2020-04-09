3053dd39f94926120d922de3dc4aebf004f580f0
ArlanPrado <arlanjprado@gmail.com>
2020-04-08 17:57:37 -0700

    Added variables and basic methods (not functional)

--- eclipse-workspace/Simstation/src/Simstation/Agent.java
+++ eclipse-workspace/Simstation/src/Simstation/Agent.java
@@ -1,5 +1,42 @@
 package Simstation;
 
-public class Agent {
 
+public abstract class Agent {
+	private String name;		//name of the agent
+	private Heading heading;	//Agent heading
+	private AgentState state;	//state
+	private int xc; 	//x coordinate
+	private int yc;		//y coordinate
+	
+	public void run() {
+		
+	}
+	public void start() {
+		
+	}
+	public void suspend() {
+		
+	}
+	public void resume() {
+		
+	}
+	public void stop() {
+		
+	}
+	public abstract void update();
+	
+	public void move(int steps) {
+		if(heading == Heading.NORTH) {
+			yc = yc - steps;
+		}
+		else if(heading == Heading.SOUTH) {
+			yc = yc + steps;
+		}
+		else if(heading == Heading.EAST) {
+			xc = xc + steps;
+		}
+		else {	//West
+			xc = xc - steps;
+		}
+	}
 }
