2efe6ac13ada19773f389f1315e95d405b7c267d
ArlanPrado <arlanjprado@gmail.com>
2020-04-08 18:03:23 -0700

    Added variables and methods to the Simulation.java

--- eclipse-workspace/Simstation/src/Simstation/Simulation.java
+++ eclipse-workspace/Simstation/src/Simstation/Simulation.java
@@ -3,5 +3,31 @@ package Simstation;
 import mvc.*;
 
 public class Simulation extends Model {
-
+	protected long clock;
+	protected String name;
+	private AgentState state;
+	private Thread thread;
+	
+	public Simulation(String name) {
+		this.name = name;
+		
+	}
+	public void start() {
+		
+	}
+	public void suspend() {
+		
+	}
+	public void resume() {
+		
+	}
+	public void stop() {
+		
+	}
+	public void getNeighbor(Agent nextAgent) {
+		
+	}
+	public void populate() {
+		
+	}
 }
